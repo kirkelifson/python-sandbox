3abd10a6ea3015b599f3824eeab96903ebf5b45a
Kirk Elifson <kirk@parodybit.net>
2014-07-27 11:46:53 -0400

    formatting

--- cover.py
+++ cover.py
@@ -6,4 +6,5 @@ rootdir = sys.argv[1]
 for root, folders, files in os.walk(rootdir):
     for directory in folders:
         files = [f for f in os.listdir(os.path.join(root, directory))]
-        if not 'cover.jpg' in files: print os.path.join(root,directory)
+        if not 'cover.jpg' in files:
+            print os.path.join(root, directory)
